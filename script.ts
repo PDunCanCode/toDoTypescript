f44eace18809d0638f4e6e632b08f261b7a67e17
pDunCanCode <chesiredarling@gmail.com>
2020-12-22 15:55:29 -0800

    add list and list.addEventListener to script.ts

--- script.ts
+++ script.ts
@@ -7,23 +7,23 @@ type Todo = {
 let todoItems: Todo[] = [];
 
 function renderTodo(todo: Todo) {
-    const list = document.querySelector(".todo-list") as HTMLInputElement;
-  
-    const isChecked = todo.checked ? "done" : "";
-    const node = document.createElement("li");
-    node.setAttribute("class", `todo-item ${isChecked}`);
-    node.setAttribute("data-key", todo.id.toString());
-    node.innerHTML = `
+  const list = document.querySelector(".todo-list") as HTMLInputElement;
+
+  const isChecked = todo.checked ? "done" : "";
+  const node = document.createElement("li");
+  node.setAttribute("class", `todo-item ${isChecked}`);
+  node.setAttribute("data-key", todo.id.toString());
+  node.innerHTML = `
       <input id="${todo.id}" type="checkbox"/>
-      <label for="${todo.id}" class="tick tick"></label>
+      <label for="${todo.id}" class="tick js-tick"></label>
       <span>${todo.text}</span>
       <button class="delete-todo delete-todo">
       <svg><use href="#delete-icon"></use></svg>
       </button>
     `;
-  
-    list.append(node);
-  }
+
+  list.append(node);
+}
 
 function addTodo(text: any) {
   const todo = {
@@ -34,6 +34,11 @@ function addTodo(text: any) {
 
   todoItems.push(todo);
   console.log(todoItems);
+}
+function toggleDone(key: number | string) {
+  const index = todoItems.findIndexById((item) => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
 }
 
 const form = document.querySelector(".form") as HTMLFormElement;
@@ -49,4 +54,10 @@ form.addEventListener("submit", (event) => {
   }
 });
 
-
+const list = document.querySelector(".todo-list") as HTMLInputElement;
+list.addEventListener("click", (event) => {
+  if (event.target.classList.contains("tick")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+});
